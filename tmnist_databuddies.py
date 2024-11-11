aca875ac4334f5efc5da2c4a6cf2eac446c7ea9d
carriedoeliseo <ecarriedo@dc.uba.ar>
2024-11-11 19:50:08 -0300

    Arreglo visualizaciones

--- tmnist_databuddies.py
+++ tmnist_databuddies.py
@@ -2,27 +2,25 @@
 # -*- coding: utf-8 -*-
 """
 
-################# LABORATORIO DE DATOS: TRABAJO PRÁCTICO 2 ####################
-
-
-########################### GRUPO E INTEGRANTES ###############################
+# ================= LABORATORIO DE DATOS: TRABAJO PRÁCTICO 2 ==================
+# ============================ GRUPO E INTEGRANTES ============================
 
 # Data Buddies:
 #    -Eliseo Carriedo  (L.U.: 392/23)
 #    -Lila Fage (L.U.: 235/24)
 #    -Julian Laurido (L.U.: 1097/23 )
 
-############################### DESCRIPCION ###################################
+# ================================ DESCRIPCION ================================
 
 #
 #
 #
 
-#%% ===========================================================================
-########################## SECCION DE IMPORTS #################################
+#%% IMPORTS ===================================================================
 
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib import ticker
 from inline_sql import sql
@@ -32,12 +30,14 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 
-#%% ===========================================================================
-########################### CARGA DE DATOS ####################################
+#%% CARGA DE DATOS ============================================================
+
 carpeta = './'
 data = pd.read_csv(carpeta+'TMNIST_Data.csv')
 columnas_num = data.select_dtypes(include=[np.int64]).columns[1:]
 data[columnas_num] = data[columnas_num].astype(np.int32)
+
+del(columnas_num)
 
 #%% ANALISIS EXPLORATORIO =====================================================
 #&& a) ========================================================================
@@ -45,7 +45,10 @@ data[columnas_num] = data[columnas_num].astype(np.int32)
 def std_data (data):
     std_data = np.std(data.iloc[:,2:], axis=0)
     img = np.array(std_data, dtype = float).reshape((28,28))
-    plt.imshow(img, cmap='gray')
+    fig, ax = plt.subplots()
+    ax.imshow(img, cmap='pink')
+    ax.set_xticks([])
+    ax.set_yticks([])
     return std_data
     
 no_importantes = std_data(data)[std_data(data) == 0].index
@@ -62,29 +65,47 @@ def plot_media_numero (data):
             data_numero = data[data['labels'] == numero].iloc[:,2:]
             data_media_numero = np.mean(data_numero, axis = 0)
             img = np.array(data_media_numero, dtype = float).reshape((28,28))
-            axs[row, col].imshow(img, cmap='gray')
+            axs[row, col].imshow(img, cmap='pink')
+            axs[row, col].set_xticks([])
+            axs[row, col].set_yticks([])
             numero += 1
 
 # Promedio intensidad de cada pixel por número
 plot_media_numero(data)
 
-data_1y3 = data[(data['labels'] == 1) | (data['labels'] == 3)].iloc[:,2:]
-data_3y8 = data[(data['labels'] == 3) | (data['labels'] == 8)].iloc[:,2:]
-
-def plot_std_1y3_3y8 (data_1y3, data_3y8):
+def plot_std_1y3_3y8 (data):
+    
+    data_1y3 = data[(data['labels'] == 1) | (data['labels'] == 4)].iloc[:,2:]
+    data_3y8 = data[(data['labels'] == 1) | (data['labels'] == 0)].iloc[:,2:]
+    
     datas = [data_1y3, data_3y8]
-    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(13,6))
-    for col in range (2):
-        std = np.std(datas[col], axis = 0)
+    fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(10,8))
+    for row in range (2):
+        std = np.std(datas[row], axis = 0)
         img = np.array(std).reshape((28,28))
-        axs[col].imshow(img, cmap='gray')
+        axs[row][0].imshow(img, cmap='pink')
+        
+        axs[row][0].set_xticks([])
+        axs[row][0].set_yticks([])
+        axs[row][0].set_title('('+str(row +1)+')', loc='left')
+        
+        sns.histplot(x=std, ax=axs[row][1], bins=20, color = 'orange')
+        axs[row][1].set_xlabel('Desviaciones estándar')
+        axs[row][1].set_ylabel('')
+        counts, _ = np.histogram(std, bins=20)
+        max_count = counts.max()
+        axs[row][1].axhline(y=max_count, color='black', linestyle='--', linewidth=0.5)
+        axs[row][1].set_xticks(np.arange(0, max(std) +1, 10))
+        axs[row][1].set_yticks([max_count])
+        
+    plt.subplots_adjust(wspace=0)
             
 # Variabilidad de intensidad entre 1 y 3 y entre 3 y 8
-plot_std_1y3_3y8(data_1y3, data_3y8)
+plot_std_1y3_3y8(data)
 
 #%% c) ========================================================================
                        
-def plot_clase_0 (data):
+def plot_numeros (data):
     nums = [0,1,4,7]
     fig, axs = plt.subplots(nrows=2, ncols=4, figsize=(13,6))
     
@@ -94,10 +115,14 @@ def plot_clase_0 (data):
         
         for row in range (2):
             img = np.array(datas[row]).reshape((28,28))
-            axs[row][col].imshow(img, cmap='gray')
-        
+            axs[row, col].imshow(img, cmap='pink')
+            axs[row, col].set_xticks([])
+            axs[row, col].set_yticks([])
+   
+    plt.subplots_adjust(wspace=0, hspace=0.1)
+    
 # Variabilidad y promedio de la clase 0
-plot_clase_0 (data)
+plot_numeros (data)
 
 #%% d) ========================================================================
 
@@ -105,10 +130,6 @@ plot_clase_0 (data)
 La exploración se complica debido a la necesidad de herramientas de 
 visualizacion.
 """
-
-#%% Borrado ===================================================================
-
-del (data_1y3, data_3y8)
 
 #%% Clasificación binaria =====================================================
 #&& a) ========================================================================
@@ -136,31 +157,54 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, shuffle
 #%% c) ========================================================================
 
 def exactitud_0y1_por_atributos (n_atributos, X_train, X_test, Y_train, Y_test):
-    exactitudes = []
+    
+    exactitudes_test = []
+    exactitudes_train = []
+    
     for N in n_atributos:
         
         modelo_0y1 = KNeighborsClassifier(n_neighbors=5)
         atributos = X_train.sample(n=N, axis=1).columns
         modelo_0y1.fit(X_train[atributos], Y_train)
 
-        Y_pred = modelo_0y1.predict(X_test[atributos])
-        exactitudes.append(round(metrics.accuracy_score(Y_test, Y_pred), 2))
+        Y_test_pred = modelo_0y1.predict(X_test[atributos])
+        exactitudes_test.append(round(metrics.accuracy_score(Y_test, Y_test_pred), 2))
         
-    iteraciones = np.arange(1, len(exactitudes)+1, 1)
+        Y_train_pred = modelo_0y1.predict(X_train[atributos])
+        exactitudes_train.append(round(metrics.accuracy_score(Y_train, Y_train_pred), 2))
+        
+    iteraciones = np.arange(1, len(exactitudes_test)+1, 1)
     fig, ax = plt.subplots()
-    ax.plot(iteraciones,exactitudes)
-
-n_atributos = [3]*10
+    
+    ax.plot(iteraciones, 
+            exactitudes_test, 
+            '-o', 
+            color = 'purple', 
+            label='Test')
+    
+    ax.plot(iteraciones, 
+            exactitudes_train, 
+            '-o', label='Train', 
+            linestyle='--', 
+            alpha=0.5, 
+            color='gray')
+    
+    ax.legend(title='Predict', loc='lower right')
+    ax.set_xlabel('Iteración')
+    ax.set_ylabel('Exactitud')
+    ax.set_xticks([3,6,9,12,15,18])
+    
+n_atributos = [3]*15
 exactitud_0y1_por_atributos (n_atributos, X_train, X_test, Y_train, Y_test)
 
-n_atributos = np.arange(3,20,1)
+n_atributos = np.arange(3,19,1)
 exactitud_0y1_por_atributos (n_atributos, X_train, X_test, Y_train, Y_test)
 
 #%% d) ========================================================================
 
 def dataExactitudes (n_atributos, k_vecinos, X_train, X_test, Y_train, Y_test):
     
-    exactitudes = pd.DataFrame(columns=['num_atributos', 'num_vecinos', 'exactitud', 'precision_0', 'recall_0', 'precision_1', 'recall_1'])
+    exactitudes = pd.DataFrame(columns=['num_atributos', 'num_vecinos', 'exactitud'])
     
     for N in n_atributos:
         for k in k_vecinos:
@@ -172,36 +216,32 @@ def dataExactitudes (n_atributos, k_vecinos, X_train, X_test, Y_train, Y_test):
             Y_pred = modelo_0y1.predict(X_test[atributos])
             
             exactitud = round(metrics.accuracy_score(Y_test, Y_pred), 2)
-            confusion = metrics.confusion_matrix(Y_test, Y_pred, labels=[0, 1])
-            
-            prec0 = round((confusion[0,0])/sum(confusion[:,0]), 2)
-            reca0 = round((confusion[0,0])/sum(confusion[0,:]), 2)
-            prec1 = round((confusion[1,1])/sum(confusion[:,1]), 2)
-            reca1 = round((confusion[1,1])/sum(confusion[1,:]), 2)
-            
-            exactitudes.loc[len(exactitudes)] = [N, k, exactitud, prec0, reca0, prec1, reca1]
+            exactitudes.loc[len(exactitudes)] = [N, k, exactitud]
             
     return exactitudes
 
     
 n_atributos = np.arange(3,16,1)
-k_vecinos = np.arange(3,11,1)
+k_vecinos = np.arange(3,16,1)
 exactitudes = dataExactitudes(n_atributos, k_vecinos, X_train, X_test, Y_train, Y_test)
 
 def exactitudes_plot (exactitudes):
     
-    fig, ax = plt.subplots(figsize=(13, 8))
+    fig, ax = plt.subplots(figsize=(10,10))
+    
     scatter = ax.scatter(exactitudes['num_vecinos'],
                          exactitudes['num_atributos'],
-                         s=exactitudes['exactitud']*1000,
+                         s=exactitudes['exactitud']*2000,
                          c=exactitudes['exactitud'],
                          cmap='viridis',
-                         alpha=0.6,
-                         edgecolors='w')
+                         alpha=0.7,
+                         marker='o')
+    
     fig.colorbar(scatter, label='Exactitud')
-    ax.set_title('Bubble Chart de Vecinos vs Atributos')
-    ax.set_label('Cantidad de Vecinos')
-    ax.set_label('Cantidad de Atributos')
+    ax.set_xlabel('Cantidad de Vecinos')
+    ax.set_ylabel('Cantidad de Atributos')
+    ax.set_xticks(np.arange(3,16,1))
+    ax.set_yticks(np.arange(3,16,1))
     ax.grid(True)
     
 exactitudes_plot(exactitudes)
@@ -224,17 +264,22 @@ X_train, X_test, Y_train, Y_test = train_test_split(X_dev, Y_dev, test_size=0.2,
 
 def exactitudes_depth_tree(X_train, X_test, Y_train, Y_test):
     
-    exactitudes_tree = pd.DataFrame(columns=['Profundidad', 'Precision'])
+    exactitudes_tree = pd.DataFrame(columns=['Train/Test', 'Profundidad', 'Exactitud'])
     
     for i in range(1,11):
         
         model_tree = DecisionTreeClassifier(criterion='entropy', max_depth=i)
         model_tree.fit(X_train, Y_train)
-        prediction_tree = model_tree.predict(X_test)
         
-        #A partir de profundidad 8, la accuracy se mantiene estable alrededor de [0.92-0.93]
+        # ------------------------------- TEST --------------------------------
+        prediction_tree = model_tree.predict(X_test)
         exactitud = round(metrics.accuracy_score(Y_test, prediction_tree), 2)
-        exactitudes_tree.loc[len(exactitudes_tree)] = [i, exactitud]
+        exactitudes_tree.loc[len(exactitudes_tree)] = ['Test', i, exactitud]
+        
+        # ------------------------------ TRAIN --------------------------------
+        prediction_tree = model_tree.predict(X_train)
+        exactitud = round(metrics.accuracy_score(Y_train, prediction_tree), 2)
+        exactitudes_tree.loc[len(exactitudes_tree)] = ['Train', i, exactitud]
         
     return exactitudes_tree
     
@@ -242,21 +287,39 @@ exactitudes_depth_tree = exactitudes_depth_tree(X_train, X_test, Y_train, Y_test
 
 def exactitud_depth_tree_plot (exactitudes_tree):
     
+    exactitudes_test = exactitudes_tree[exactitudes_tree['Train/Test'] == 'Test']
+    exactitudes_train = exactitudes_tree[exactitudes_tree['Train/Test'] == 'Train']
+    
     fig, ax = plt.subplots()
-    ax.plot(exactitudes_tree['Profundidad'], exactitudes_tree['Precision'])
+    
+    ax.plot(exactitudes_test['Profundidad'], 
+            exactitudes_test['Exactitud'], 
+            '-o', 
+            color = 'purple',
+            label='Test')
+    
+    ax.plot(exactitudes_train['Profundidad'], 
+            exactitudes_train['Exactitud'], 
+            '-o', linestyle='--', 
+            alpha=0.5, 
+            color='gray', 
+            label='Train')
+    
     ax.set_xlabel('Profundidades')
-    ax.set_ylabel('Precision del Arbol')
+    ax.set_ylabel('Exactitud')
+    ax.legend(title='Predict', loc='lower right')
     ax.set_xticks(np.arange(1,11,1))
     
 exactitud_depth_tree_plot(exactitudes_depth_tree)
 
 #%% c) ========================================================================
+#&& Carga de información ======================================================
 
 def exactitudes_hiperparametros (X_train, X_test, Y_train, Y_test):
     
-    maxAtributos = np.arange(2, 15, 3)
-    profundidades = np.arange(1, 10, 2)
-    minEjemplares = np.arange(2, 10, 2)
+    maxAtributos = np.arange(5, 11, 1)
+    profundidades = np.arange(5, 11, 1)
+    minEjemplares = [2,4,8,16,32]
 
     exactitudes = pd.DataFrame(columns=['Train/Test','Criterio', 'Hiperparametro', 'Iteracion', 'Exactitud'])
     hiperparametros = ['MaxFeatures', 'Profundidad', 'minSamples']
@@ -376,7 +439,7 @@ def k_folding (nsplits, X_dev, Y_dev):
 
 exactitudes_foldings = k_folding (5, X_dev, Y_dev)
 
-#%%
+#%% Ploteo de información =====================================================
 
 exactitudes_promedios_train = sql^ """
                              SELECT Criterio, 
@@ -407,7 +470,7 @@ exactitudes_promedios_test = sql^ """
 def plot_hiperparametros (exactitudes_promedios_train, exactitudes_promedios_test):
     
     hiperparametros = ['MaxFeatures', 'Profundidad', 'minSamples']
-    fig, ax = plt.subplots(ncols=1, nrows=3, figsize=(8,10))
+    fig, ax = plt.subplots(ncols=1, nrows=3, figsize=(10,15))
     i = 0
     for h in hiperparametros:
         
@@ -439,31 +502,55 @@ def plot_hiperparametros (exactitudes_promedios_train, exactitudes_promedios_tes
                                  
          ax[i].plot(exactitudes_entr_train['Iteracion'], 
                     exactitudes_entr_train['AVG_Exactitud_Train'],
-                    linestyle='--', 
-                    color='green',
+                    '-o',
+                    linestyle='--',
+                    label='Entropy Train',
+                    color='thistle',
                     alpha = 0.5)
          
          ax[i].plot(exactitudes_gini_train['Iteracion'], 
                     exactitudes_gini_train['AVG_Exactitud_Train'],
                     'o-',
                     linestyle='--', 
-                    color='red',
+                    label='Gini Train',
+                    color='moccasin',
                     alpha = 0.5)
          
          ax[i].plot(exactitudes_entr_test['Iteracion'], 
                     exactitudes_entr_test['AVG_Exactitud_Test'],
-                    'o-',
-                    linestyle='-', 
-                    color='green',
+                    'o-', 
+                    label='Entropy Test',
+                    color='purple',
                     alpha = 0.5)
          
          ax[i].plot(exactitudes_gini_test['Iteracion'], 
                     exactitudes_gini_test['AVG_Exactitud_Test'],
                     'o-',
-                    linestyle='-', 
-                    color='red',
+                    label='Gini Test',
+                    color='darkorange',
                     alpha = 0.5)
          
+         if h == 'MaxFeatures':
+             ax[i].set_title('(1)', loc='right')
+             ax[i].set_xlabel('Cantidad de Atributos')
+             ax[i].set_ylabel('Exactitud')
+             ax[i].set_xticks(np.arange(5, 11, 1))
+             
+         elif h == 'Profundidad':
+             ax[i].set_title('(2)', loc='right')
+             ax[i].set_xlabel('Profundidad del árbol')
+             ax[i].set_ylabel('Exactitud')
+             ax[i].set_xticks(np.arange(5, 11, 1))
+             
+         elif h == 'minSamples':
+             ax[i].set_title('(3)', loc='right')
+             ax[i].set_xlabel('Minimo de Ejemplares')
+             ax[i].set_ylabel('Exactitud')
+             ax[i].set_xticks([2,4,8,16,32])
+         
+         fig.legend(['Entropy Train', 'Gini Train', 'Entropy Test', 'Gini Test'],
+                    bbox_to_anchor=(0.275, 1.07))
+         plt.subplots_adjust(top=1)
          i+=1
          
     plt.show()
